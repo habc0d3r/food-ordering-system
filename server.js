5e5c2f2ba90c2b92e48d93b88301e940f49d4c49
uddiptagogoi2000 <uddiptagogoi2000@gmail.com>
2020-12-16 20:39:05 +0530

    connect to mongodb

--- server.js
+++ server.js
@@ -1,10 +1,33 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const ejs = require('ejs')
 const path = require('path')
 const expressEjsLayouts = require('express-ejs-layouts')
 const PORT = process.env.PORT || 3000
+const mongoose = require('mongoose')
+const session = require('express-session')
 
+//* Database connection
+const url = 'mongodb://localhost/pizza';
+
+mongoose.connect(url, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: true});
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log('Database connected...');
+}).catch(err => {
+    console.log('Connection failed...')
+});
+
+//* session config
+app.use(session({
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 1000 * 60 * 60 * 24 } //* 24 hours
+}))
+
+//* assets
 app.use(express.static('public'))
 
 //* set template engine
@@ -12,21 +35,7 @@ app.use(expressEjsLayouts)
 app.set('views', path.join(__dirname + '/resources/views'))
 app.set('view engine', 'ejs')
 
-app.get("/", (req, res) => {
-    res.render('home')
-})
-
-app.get("/cart", (req, res) => {
-    res.render('customers/cart')
-})
-
-app.get("/login", (req, res) => {
-    res.render('auth/login.ejs')
-})
-
-app.get("/register", (req, res) => {
-    res.render('auth/register.ejs')
-})
+require('./routes/web')(app)
 
 app.listen(PORT, () => {
     console.log(`Server has started on port ${PORT}`);
