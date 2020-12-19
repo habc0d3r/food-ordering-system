8c1c48a343a8624ea7d776cf683a75d59e4fe7c9
uddiptagogoi2000 <uddiptagogoi2000@gmail.com>
2020-12-19 21:46:14 +0530

    add cart counter when add to cart

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const expressEjsLayouts = require('express-ejs-layouts')
 const PORT = process.env.PORT || 3000
 const mongoose = require('mongoose')
 const session = require('express-session')
+const flash = require('express-flash')
+const MongoDbStore = require('connect-mongo')(session)
 
 //* Database connection
 const url = 'mongodb://localhost/pizza';
@@ -19,16 +21,32 @@ connection.once('open', () => {
     console.log('Connection failed...')
 });
 
+//* Session store
+let mongoStore = new MongoDbStore({
+               mongooseConnection: connection,
+               collection: 'sessions'
+            })
+
 //* session config
 app.use(session({
     secret: process.env.COOKIE_SECRET,
     resave: false,
+    store: mongoStore,
     saveUninitialized: false,
     cookie: { maxAge: 1000 * 60 * 60 * 24 } //* 24 hours
 }))
 
+app.use(flash())
+
 //* assets
 app.use(express.static('public'))
+app.use(express.json())
+
+//* Global Middleware
+app.use((req, res, next) => {
+    res.locals.session = req.session
+    next()
+})
 
 //* set template engine
 app.use(expressEjsLayouts)
