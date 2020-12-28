0fc00d9ded520d7944ec4d372ab3c7be04e85a69
uddiptagogoi2000 <uddiptagogoi2000@gmail.com>
2020-12-28 21:47:50 +0530

    auth system

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const mongoose = require('mongoose')
 const session = require('express-session')
 const flash = require('express-flash')
 const MongoDbStore = require('connect-mongo')(session)
+const passport = require('passport')
 
 //* Database connection
 const url = 'mongodb://localhost/pizza';
@@ -36,15 +37,23 @@ app.use(session({
     cookie: { maxAge: 1000 * 60 * 60 * 24 } //* 24 hours
 }))
 
+//* Passport config
+const passportInit = require('./app/config/passport')
+passportInit(passport)
+app.use(passport.initialize())
+app.use(passport.session())
+
 app.use(flash())
 
 //* assets
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
 //* Global Middleware
 app.use((req, res, next) => {
     res.locals.session = req.session
+    res.locals.user = req.user
     next()
 })
 
