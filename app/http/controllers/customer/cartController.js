e1d75cb4397261670d94c85a24f0ab4345aed474
Uddipta Gogoi <uddiptagogoi2000@gmail.com>
2021-07-27 23:01:18 +0530

    add remove item from cart functionality

--- app/http/controllers/customer/cartController.js
+++ app/http/controllers/customer/cartController.js
@@ -1,35 +1,46 @@
 function cartController() {
-    return {
-        index(req, res) {
-            res.render('customers/cart')
-        },
-        update(req, res) {
-            //* for the first time creating cart and adding basic object structure
-            if (!req.session.cart) {
-                req.session.cart = {
-                    items: {},
-                    totalQty: 0,
-                    totalPrice: 0
-                }
-            }
-            let cart = req.session.cart
-            // Check if item does not exist in CART
-            if (!cart.items[req.body._id]) {
-                cart.items[req.body._id] = {
-                    item: req.body,
-                    qty: 1
-                }
-                cart.totalQty += 1
-                cart.totalPrice = cart.totalPrice + req.body.price
-            } else {
-                cart.items[req.body._id].qty = cart.items[req.body._id].qty + 1
-                cart.totalQty = cart.totalQty + 1
-                cart.totalPrice = cart.totalPrice + req.body.price
-            }
-            return res.json({ totalQty: req.session.cart.totalQty })
+  return {
+    index(req, res) {
+      res.render('customers/cart')
+    },
+    update(req, res) {
+      //* for the first time creating cart and adding basic object structure
+      if (!req.session.cart) {
+        req.session.cart = {
+          items: {},
+          totalQty: 0,
+          totalPrice: 0,
         }
-    }
-}
+      }
+      let cart = req.session.cart
+      // Check if item does not exist in CART
+      if (!cart.items[req.body._id]) {
+        cart.items[req.body._id] = {
+          item: req.body,
+          qty: 1,
+        }
+        cart.totalQty += 1
+        cart.totalPrice = cart.totalPrice + req.body.price
+      } else {
+        cart.items[req.body._id].qty = cart.items[req.body._id].qty + 1
+        cart.totalQty = cart.totalQty + 1
+        cart.totalPrice = cart.totalPrice + req.body.price
+      }
+      return res.json({ totalQty: req.session.cart.totalQty })
+    },
+    async delete(req, res) {
+      const _id = req.params.id
 
+      req.session.cart.totalQty -= req.session.cart.items[_id].qty
+      req.session.cart.totalPrice -=
+        req.session.cart.items[_id].item.price * req.session.cart.items[_id].qty
+
+      await delete req.session.cart.items[_id]
+
+      console.log(req.session.cart.items)
+      res.status(302).redirect('/cart')
+    },
+  }
+}
 
 module.exports = cartController
--- routes/web.js
+++ routes/web.js
@@ -11,24 +11,25 @@ const auth = require('../app/http/middlewares/auth')
 const admin = require('../app/http/middlewares/admin')
 
 function initRoutes(app) {
-    app.get("/", homeController().index)
-    app.get("/login", guest, authController().login)
-    app.post("/login", authController().postLogin)
-    app.get("/register", guest, authController().register)
-    app.post("/register", authController().postRegister)
-    app.post("/logout", authController().logout)
+  app.get('/', homeController().index)
+  app.get('/login', guest, authController().login)
+  app.post('/login', authController().postLogin)
+  app.get('/register', guest, authController().register)
+  app.post('/register', authController().postRegister)
+  app.post('/logout', authController().logout)
 
-    app.get("/cart", cartController().index)
-    app.post("/update-cart", cartController().update)
+  app.get('/cart', cartController().index)
+  app.post('/update-cart', cartController().update)
+  app.post('/cart/remove/:id', cartController().delete)
 
-    //customer routes
-    app.post('/orders', auth, orderController().store)
-    app.get('/customer/orders', auth, orderController().index)
-    app.get('/customer/orders/:id', auth, orderController().show)
+  //customer routes
+  app.post('/orders', auth, orderController().store)
+  app.get('/customer/orders', auth, orderController().index)
+  app.get('/customer/orders/:id', auth, orderController().show)
 
-    //Admin routes
-    app.get('/admin/orders', admin, adminOrderController().index)
-    app.post('/admin/orders/status', admin, statusController().update)
+  //Admin routes
+  app.get('/admin/orders', admin, adminOrderController().index)
+  app.post('/admin/orders/status', admin, statusController().update)
 }
 
 module.exports = initRoutes
